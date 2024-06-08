9c3063d8a82dff41a920c1ee2d39bcf96c3b9c59
Lee Houk <leehouk@gmail.com>
2024-06-08 15:05:35 -0700

    Added Environtment

--- braceletempire.client/src/app/services/order.service.ts
+++ braceletempire.client/src/app/services/order.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order } from '../interfaces/TempOrder';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-  private apiUrl = '/api/order'; // Update with your actual API URL
+  private apiUrl = environment.apiUrl; // Update with your actual API URL
 
   constructor(private http: HttpClient) { }
 
   createOrder(order: Order): Observable<number> {
-    return this.http.post<number>(this.apiUrl, order);
+    return this.http.post<number>(`${this.apiUrl}/Order`, order);
   }
 
   getOrder(orderId: number): Observable<Order> {
-    return this.http.get<Order>(`${this.apiUrl}/${orderId}`);
+    return this.http.get<Order>(`${this.apiUrl}/Order/${orderId}`);
   }
 }
